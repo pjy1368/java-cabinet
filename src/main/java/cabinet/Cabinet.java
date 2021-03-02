77aba46e04a19448ae8eb2d7e90831991f804c30
JinYoung Park <basejin0810@naver.com>
2021-03-02 13:29:30 +0900

    feat: 초기 세팅

---
+++ src/main/java/cabinet/Cabinet.java
@@ -0,0 +1,10 @@
+package cabinet;
+
+import java.util.List;
+
+public class Cabinet {
+
+    public static List<String> allocate(String teamMember, int exclude) {
+        return null;
+    }
+}
---
+++ src/test/java/cabinet/CabinetTest.java
@@ -0,0 +1,18 @@
+package cabinet;
+
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+class CabinetTest {
+    @Test
+    void allocate() {
+        List<String> result = Cabinet.allocate("15:춘식,17:에어", 40);
+        assertThat(result).hasSize(52);
+//        assertThat(result.get(14)).isEqualTo("춘식");
+//        assertThat(result.get(16)).isEqualTo("에어");
+//        assertThat(result.get(39)).isEqualTo("X");
+//        showResult(result);
+    }
+}
