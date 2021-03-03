d89fe460a9cc65ae9cfdee3017c85fe7175ea77b
pjy1368 <basejin0810@naver.com>
2021-03-03 10:13:18 +0900

    feat(Cabinet): 특정 좌석을 할당하는 기능 구현

--- src/main/java/cabinet/Cabinet.java
+++ src/main/java/cabinet/Cabinet.java
@@ -1,10 +1,20 @@
 package cabinet;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Cabinet {
 
-    public static List<String> allocate(String teamMember, int exclude) {
-        return null;
+    public static List<String> allocate(String memberInfo, int exclude) {
+        final List<String> members = new ArrayList<>(Arrays.asList(new String[52]));
+        members.set(exclude - 1, "X");
+        String[] teamMembers = memberInfo.split(",");
+        for (String teamMember : teamMembers) {
+            int idx = Integer.parseInt(teamMember.split(":")[0]) - 1;
+            String name = teamMember.split(":")[1];
+            members.set(idx, name);
+        }
+        return members;
     }
 }
